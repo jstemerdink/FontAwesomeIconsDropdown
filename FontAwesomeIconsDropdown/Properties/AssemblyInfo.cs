b8f39b977b2d337776c34b0a28a08370ac1084fe
Jeroen <jeroen.developer@outlook.com>
2016-05-05 14:20:20 +0200

    Updated assembly info

--- FontAwesomeIconsDropdown/Properties/AssemblyInfo.cs
+++ FontAwesomeIconsDropdown/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FontAwesomeIconsDropdown")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is an EPiServer edit-mode dropdown that displays the list of font-awesome icons to the editor.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Marija Jemuovic")]
 [assembly: AssemblyProduct("FontAwesomeIconsDropdown")]
 [assembly: AssemblyCopyright("Copyright © Marija Jemuovic, Jeroen Stemerdink 2016")]
 [assembly: AssemblyTrademark("")]
